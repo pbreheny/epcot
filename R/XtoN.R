b34793fd771a3fdefb0efa9c3a94cc883295badb
Patrick Breheny <patrick-breheny@uiowa.edu>
2017-05-04 14:47:12 -0500

    Fixed devastating bug in XtoN

--- R/XtoN.R
+++ R/XtoN.R
@@ -1,12 +1,16 @@
 XtoN <- function(X, fam) {
-  Fam <- splitleft(fam[,1], "_")
-  nonPairs <- which(Fam %in% names(which(table(Fam)!=2)))
+  fam$V3 <- splitleft(fam$V3, "-")
+  fam$V1 <- splitleft(fam$V1, "_")
+  #nonPairs <- which(fam$V1 %in% names(which(table(fam$V1)!=2)))
+  #npID <- fam[nonPairs, "V3"]
+  Pairs <- which(fam$V1 %in% names(which(table(fam$V1)==2)))
+  pID <- fam[Pairs, "V3"]
 
   ## Convert to 0/1/2 counts
   N <- matrix(NA, nrow(X), 3, dimnames=list(rownames(X), 0:2))
   pb <- txtProgressBar(1, nrow(X), style=3)
   for (i in 1:nrow(X)) {
-    famCounts <- tapply(X[i,-nonPairs]!=0, Fam[-nonPairs], sum)
+    famCounts <- tapply(X[i,pID]!=0, subset(fam, V3 %in% pID)$V1, sum)
     N[i,1] <- sum(famCounts==0)
     N[i,2] <- sum(famCounts==1)
     N[i,3] <- sum(famCounts==2)
