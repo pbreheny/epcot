7d912e100c68d9774219ccb4d6d7efad41e6c26c
Patrick Breheny <patrick-breheny@uiowa.edu>
2017-06-03 07:03:43 -0500

    showGene: vData then N instead of other way around

--- R/showGene.R
+++ R/showGene.R
@@ -1,4 +1,4 @@
 showGene <- function(Gene, N, vData, cutoff=0.05) {
   ind <- which(vData$Gene==Gene & vData$ExAC < cutoff)
-  cbind(N[ind,,drop=FALSE], vData[ind,])
+  cbind(vData[ind,], N[ind,,drop=FALSE])
 }
