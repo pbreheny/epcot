8bff0094ab698e3e6e47a4a970cf42fd80853588
Patrick Breheny <patrick-breheny@uiowa.edu>
2018-07-13 11:26:04 -0500

    0.3-0: bayesTest now includes null simulation q-values

--- R/bayesTest.R
+++ R/bayesTest.R
@@ -1,5 +1,16 @@
-bayesTest <- function(N.pair, N.trio, vData, cutoff=0.2) {
-  ind <- vData$ExAC < 0.2
+#' Bayes test of excess variant frequency with CADD-based prior
+#'
+#' @param N.pair   Matrix of 0/1/2 counts for sister pairs
+#' @param N.trio   Matrix of 0/1/2/3 counts for sister trios
+#' @param vData    data.table of variant info (ExAC frequencies and CADD scores)
+#' @param cutoff   Exclude variants with ExAC frequencies above this cutoff
+#'
+#' @examples bayesTest(N.pair, N.trio, vData)
+
+bayesTest <- function(N.pair, N.trio, vData, cutoff=0.2, ceiling=2000, floor=0.01, rate=0.5, Q=100) {
+
+  # Setup
+  ind <- vData$ExAC < cutoff
   if (!sum(ind)) return(NA)
   N.pair <- N.pair[ind,,drop=FALSE]
   N.trio <- N.trio[ind,,drop=FALSE]
@@ -8,7 +19,9 @@ bayesTest <- function(N.pair, N.trio, vData, cutoff=0.2) {
   ntrio <- apply(N.trio, 1, sum) #sum across the rows. gives # of total trios
   if (missing(vData)) vData <- get("vData", .GlobalEnv)
   CADD <- vData$CADD
-  prior <- 10^(-(CADD/10-3))
+
+  # Determine prior
+  tau <- ceiling * exp(-rate * CADD) + floor
 
   # Calculate Exp
   npair <- apply(N.pair, 1, sum)
@@ -24,7 +37,17 @@ bayesTest <- function(N.pair, N.trio, vData, cutoff=0.2) {
   # Exp, Obs
   Exp <- npair*probPairs + ntrio*(probTrios2+probTrios3)  #expected number of pairs w/ both sisters having variant and trios w/ at least 2 sisters having variant
   Obs <- N.pair[,3]+N.trio[,3]+N.trio[,4]
+  Prob <- 1-pgamma(1, Obs + tau, Exp + tau)
 
-  # Return
-  data.frame(Obs, Exp, Prob=1-pgamma(1, Obs + prior, Exp + prior))
+  # qval
+  if (Q > 0) {
+    nv <- length(Exp)
+    NN <- matrix(rpois(nv*Q, Exp), nv, Q)
+    PP <- matrix(1-pgamma(1, NN + tau, Exp + tau), nv, Q)
+    null <- ecdf(PP)
+    q <- pmin(nv*(1-null(Prob))/rank(1-Prob), 1)
+    return(data.frame(Obs, Exp, Prob, q))
+  } else {
+    return(data.frame(Obs, Exp, Prob))
+  }
 }
---
+++ R/qval.R
@@ -0,0 +1,3 @@
+qval <- function() {
+
+}
