2878781d1d9701d03f46ee1236803da3dacd6a09
Patrick Breheny <patrick-breheny@uiowa.edu>
2020-06-12 14:11:34 -0500

    Renaming test directory

--- tests/test.R
+++ tests/test.R
@@ -1,4 +1,4 @@
 if (requireNamespace("tinytest", quietly=TRUE)) {
   home <- length(unclass(packageVersion("epcot"))[[1]]) == 4
-  if (home) tinytest::test_package("epcot", "tests")
+  if (home) tinytest::test_package("epcot")
 }
